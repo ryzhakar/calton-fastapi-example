67d0bcd936e31e3e060e4a40c54028a7747b41fa
Arthur Ryzhak <ryzhakar@gmail.com>
2024-07-18 21:36:15 +0300

    feat: add endpoint to add a review

--- app/routers/reviews.py
+++ app/routers/reviews.py
@@ -2,11 +2,13 @@ from typing import Annotated
 
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi.responses import Response
 
 from app.datasources import MemoryXLSXDatasource
 from app.interface import schemas
 
 router = APIRouter(prefix='/reviews')
+_CREATED_STATUS_CODE = 201
 
 
 @router.get('/', response_model=schemas.MultipleReviewsResponse)
@@ -17,3 +19,13 @@ def fetch_reviews(
     """Return a slice of the reviews."""
     reviews = datasource.list_multiple_reviews_with(pagination)
     return schemas.MultipleReviewsResponse(reviews=reviews)
+
+
+@router.post('/')
+def add_review(
+    datasource: Annotated[MemoryXLSXDatasource, Depends()],
+    review_body: schemas.Review,
+) -> Response:
+    """Add a review to the datasource."""
+    datasource.add_reviews(review_body)
+    return Response(status_code=_CREATED_STATUS_CODE)
