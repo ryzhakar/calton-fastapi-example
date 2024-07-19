fe08463dfd5e30a491877b7053df5789ce2c2b04
Arthur Ryzhak <ryzhakar@gmail.com>
2024-07-19 22:20:48 +0300

    feat(api): add scraping reader endpoint

--- app/routers/reviews.py
+++ app/routers/reviews.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi.responses import Response
 
+from app.datasources import JustEatDataSource
 from app.datasources import MemoryXLSXDatasource
 from app.interface import schemas
 
@@ -29,3 +30,14 @@ async def add_review(
     """Add a review to the datasource."""
     datasource.add_reviews(review_body)
     return Response(status_code=_CREATED_STATUS_CODE)
+
+
+@router.get('/scrape/justeat', response_model=schemas.MultipleReviewsResponse)
+async def scrape_justeat(
+    datasource: Annotated[JustEatDataSource, Depends()],
+    pagination: Annotated[schemas.PaginationOptions, Depends()],
+) -> schemas.MultipleReviewsResponse:
+    """Scrape reviews from Just Eat."""
+    async with datasource:
+        reviews = await datasource.get_reviews(pagination)
+    return schemas.MultipleReviewsResponse(reviews=reviews)
